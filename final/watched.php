ef6cf302bfc6d1ece3f002c7c5d0f843ee06cb6c
nfalkj01 <114940991+nfalkj01@users.noreply.github.com>
2023-04-23 21:18:22 -0400

    watched update

--- final/watched.php
+++ final/watched.php
@@ -47,10 +47,483 @@ session_start();
 	}	
 	?>
 	<script>
-		// display here
-		document.write(JSON.stringify(arr))
-		console.log(arr);
-	</script>
+
+function get_cast_info(movie_id, i) {
+let URL = "https://api.themoviedb.org/3/movie/"+movie_id+"/credits?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US";
+	res = fetch(URL)
+		.then (res => res.text())
+		.then (data => 
+		{
+			var people = JSON.parse(data);
+			var cast_length = Object.keys(people["cast"]).length;
+			//console.log("people lenght: " + cast_length);
+			
+			if (cast_length == 0) {
+				document.getElementById("cast" + i).innerHTML += "<li id='cast_item0'> No cast information found at this time. </li>";
+			}
+			else {
+				for (let k = 0; k < cast_length; k++) {
+					if (people["cast"][k].known_for_department == "Acting") {
+						document.getElementById("cast" + i).innerHTML += "<li id='cast_item" + k + "'> " + people["cast"][k].name + "</li>";
+					}
+				}
+			}
+		})
+		.catch (error => console.log(error))
+
+}
+
+function showCast(i) {
+var x = document.getElementById("cast" + i);
+var button = document.getElementById("cast_button");
+if (button.innerHTML == "Click to see cast info!") {
+	x.style.display = "grid";
+	button.innerHTML = "Click to close cast info!";
+}
+else {
+	x.style.display = "none";
+	button.innerHTML = "Click to see cast info!";
+}
+}
+
+
+
+function add_to_watched(i, movie_id) {
+	<?php
+	if (isset($_SESSION['userid'])){
+		echo 'user_id = "'.$_SESSION['userid'].'";';
+	}
+	else {
+		echo 'alert("Please log in to add to your watched list");';
+		echo "return;";
+	}
+	?>
+	//console.log("clicked" + i);
+	console.log("$('#add_to_watched'):eq(i).html(): " + $("#add_to_watched" + i).html());
+	if ($("#add_to_watched" + i).html() == "Add to watched") {
+		$("#add_to_watched" + i).html("Remove from watched");
+		$("#add_to_watched" + i).css("background-color","red");
+		$("#add_to_wishlist" + i).css("display","none");
+		$("#fav" + i).css("display","block");
+		$.ajax({
+			url: "addtowatch.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+		
+	}
+	else if ($("#add_to_watched" + i).html() == "Remove from watched") {
+
+		$("#add_to_watched" + i).html("Add to watched");
+		$("#add_to_watched" + i).css("background-color","#E84855");
+		$("#add_to_wishlist" + i).css("display","block");
+		$("#fav" + i).html("Add to favourites");
+		$("#fav" + i).css("display","none");
+		$.ajax({
+			url: "removefromwatch.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+		$.ajax({
+			url: "removefromfavs.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+
+	}
+
+	
+		
+}
+
+
+function add_to_fav(i, movie_id) {
+	<?php
+	if (isset($_SESSION['userid'])){
+		echo 'user_id = "'.$_SESSION['userid'].'";';
+	}
+	else {
+		echo 'alert("Please log in to add to your favorites");';
+		echo "return;";
+	}
+	?>
+	if ($("#fav" + i).html() == "Add to favourites") {
+		console.log("clicked fav")
+		$("#fav" + i).html("Remove from favourites");
+		$("#fav" + i).css("background-color","red");
+		$.ajax({
+			url: "addtofaves.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+	}
+	else if ($("#fav" + i).html() == "Remove from favourites") {
+		$("#fav" + i).html("Add to favourites");
+		$("#fav" + i).css("background-color","#AFE1AF");
+		$.ajax({
+			url: "removefromfavs.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+	}
+
+	
+}
+
+function add_to_wishlist(i, movie_id) {
+	<?php
+	if (isset($_SESSION['userid'])){
+		echo 'user_id = "'.$_SESSION['userid'].'";';
+	}
+	else {
+		echo 'alert("Please log in to add to your wishlist");';
+		echo "return;";
+	}
+	?>
+	if ($("#add_to_wishlist" + i).html() == "Add to wishlist") {
+		$("#add_to_wishlist" + i).html("Remove from wishlist");
+		$("#add_to_wishlist" + i).css("background-color","red");
+		$("#add_to_watched" + i).css("display","none");
+		$.ajax({
+			url: "addtowish.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+	}
+	else if ($("#add_to_wishlist" + i).html() == "Remove from wishlist") {
+
+		$("#add_to_wishlist" + i).html("Add to wishlist");
+		$("#add_to_wishlist" + i).css("background-color","#00CFC1");
+		$("#add_to_watched" + i).css("display","block");
+		$.ajax({
+			url: "removefromwish.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			}
+
+		});
+	}
+
+	
+	
+}
+// const wishlist_id = [299534, 299536];
+// const watched_id = [];
+var recommend_id = [];
+var similar_id = [];
+
+async function getJson_recommend(url) {
+	let response = await fetch(url);
+	let data = await response.json()
+	return data;
+}
+
+async function main_recommend(page) {
+	for (let i = 0; i < wishlist_id.length; i++) {
+		let apiUrl = "https://api.themoviedb.org/3/movie/" + wishlist_id[i] + "/recommendations?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + 1;
+		
+		var my_data = await getJson_recommend(apiUrl);
+		var obj = my_data["results"];
+		for (let k = 1; k <= my_data["total_pages"]; k ++) {
+			let apiUrl = "https://api.themoviedb.org/3/movie/" + wishlist_id[i] + "/recommendations?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + k;
+		
+			var my_data = await getJson_recommend(apiUrl);
+			var obj = my_data["results"];
+
+			for (let k = 0; k < obj.length; k++) {
+				recommend_id.push(obj[k]["id"]);
+			}
+		}
+	}
+	for (let i = 0; i < watched_id.length; i++) {
+		let apiUrl = "https://api.themoviedb.org/3/movie/" + watched_id[i] + "/recommendations?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + 1;
+		
+		var my_data = await getJson_recommend(apiUrl);
+		var obj = my_data["results"];
+		for (let k = 1; k <= my_data["total_pages"]; k ++) {
+			let apiUrl = "https://api.themoviedb.org/3/movie/" + watched_id[i] + "/recommendations?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + k;
+		
+			var my_data = await getJson_recommend(apiUrl);
+			var obj = my_data["results"];
+
+			for (let k = 0; k < obj.length; k++) {
+				recommend_id.push(obj[k]["id"]);
+			}
+		}
+	}
+
+	return recommend_id;
+	
+}
+
+async function getJson_similar(url) {
+	let response = await fetch(url);
+	let data = await response.json()
+	return data;
+}
+
+async function main_similar(page) {
+	for (let i = 0; i < wishlist_id.length; i++) {
+		var apiUrl = "https://api.themoviedb.org/3/movie/" + wishlist_id[i] + "/similar?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + page;
+		
+		var my_data = await getJson_recommend(apiUrl);
+		var obj = my_data["results"];
+			for (let k = 0; k < obj.length; k++) {
+				similar_id.push(obj[k]["id"]);
+			}
+	}
+	for (let i = 0; i < watched_id.length; i++) {
+		let apiUrl = "https://api.themoviedb.org/3/movie/" + watched_id[i] + "/similar?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US&page=" + page;
+		
+		var my_data = await getJson_recommend(apiUrl);
+		var obj = my_data["results"];
+
+
+			for (let k = 0; k < obj.length; k++) {
+				//console.log("pushing: " + obj[k]["id"]);
+				similar_id.push(obj[k]["id"]);
+			}
+		}
+
+	return similar_id;
+}
+
+
+
+async function main() {
+	while (recommend_id.length < 30) {
+		await main_recommend(1);
+	}
+
+	console.log("recommend: " + recommend_id);
+
+	let uniqueRec = [...new Set(recommend_id)];
+	
+	
+	let page_num = 1;
+	
+	while (uniqueRec.length < 40) {
+		await main_similar(page_num);
+		uniqueRec = uniqueRec.concat(similar_id);
+		uniqueRec = [...new Set(uniqueRec)];
+		page_num++;
+	}
+	console.log("final rec here: " + uniqueRec);
+	console.log("final rec size here: " + uniqueRec.length);
+
+	var whole_array = watched_id.concat(wishlist_id);
+	console.log("whole array size: " + whole_array);
+
+	for (let t = 0; t < whole_array.length; t++) {
+		for (let f = 0; f < uniqueRec.length; f++) {
+			if (whole_array[t] == uniqueRec[f]) {
+				const x = uniqueRec.splice(f, 1);
+			}
+		}
+	}
+
+	
+	console.log("similar: " + similar_id);
+	console.log("final rec after: " + uniqueRec);
+	console.log("final rec size now: " + uniqueRec.length);
+
+	function findDuplicates(arr) {
+		return arr.filter((currentValue, currentIndex) =>
+		arr.indexOf(currentValue) !== currentIndex);
+	}
+
+	//const array = [...uniqueRec];
+
+	getAPI(uniqueRec);
+}
+
+
+
+
+
+function output(movie_id, i, title, img_source, genres, overview, date) {
+document.getElementById("show_data").innerHTML += "<div id='movie" + i + "' style='border: 1px solid black'>"
+document.getElementById("movie" + i).innerHTML += "<div id='image_column'> <img id='poster' src='"+"http://image.tmdb.org/t/p/w500/" + img_source + "'> </div>";
+
+document.getElementById("movie" + i).innerHTML += "<div id='info_column'><h2 id='title'> " + title + "</h2> <p id='date'> <strong>Release Date: </strong>" + date + "</p><p id='genres'><strong>Genres: </strong>" + genres + "</p> <p id='overview'> <strong>Overview: </strong>" + overview + "</p></div>";
+
+document.getElementById("movie" + i).innerHTML += "<button onclick=\"showCast(" + i + ")\" id='cast_button'>Click to see cast info!</button><div id=\"list_cast\"><ul id='cast" + i + "'>";
+get_cast_info(movie_id, i);
+document.getElementById("movie" + i).innerHTML += "</ul></div>"
+document.getElementById("movie" + i).innerHTML += "<div id='add_buttons'><button id='add_to_watched" +i + "' onclick='add_to_watched(" + i + ", " + movie_id + ")'>Add to watched</button><button id='add_to_wishlist" + i + "' onclick='add_to_wishlist(" + i + ", " + movie_id + ")'>Add to wishlist</button><button id='fav" + i + "' onclick='add_to_fav(" + i + ", " + movie_id + ")'>Add to favourites</button></div>";
+	document.getElementById("show_data").innerHTML += "</div>"
+	var isWatch = false;
+	<?php
+				if (isset($_SESSION['userid'])){
+					echo 'user_id = "'.$_SESSION['userid'].'";';
+				}
+				else {
+					echo 'alert("Please log in to add to your wishlist");';
+					echo "return;";
+				}
+			?>
+
+	$.ajax({
+			url: "iswatchpresent.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			},
+			success: function(response){
+				if (response=='true'){
+					$("#add_to_watched" + i).html("Remove from watched");
+					$("#add_to_watched" + i).css("background-color","red");
+					$("#add_to_wishlist" + i).css("display","none");
+					$("#fav" + i).css("display","block");
+					isWatch = true;
+				}
+			}
+
+});
+<?php
+			if (isset($_SESSION['userid'])){
+				echo 'user_id = "'.$_SESSION['userid'].'";';
+			}
+			else {
+				echo 'alert("Please log in to add to your wishlist");';
+				echo "return;";
+			}
+			?>
+
+$.ajax({
+			url: "isfavepresent.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			},
+			success: function(response){
+				if (response=='true'){
+					$("#fav" + i).html("Remove from favourites");
+					$("#fav" + i).css("background-color","red");
+				}
+			}
+
+});
+<?php
+			if (isset($_SESSION['userid'])){
+				echo 'user_id = "'.$_SESSION['userid'].'";';
+			}
+			else {
+				echo 'alert("Please log in to add to your wishlist");';
+				echo "return;";
+			}
+			?>
+
+$.ajax({
+
+	url: "iswishpresent.php",
+			type: "POST",
+			data: {
+				movieid: movie_id,
+				userid: user_id
+			},
+			success: function(response){
+				if (response=='true' && isWatch==false){
+					$("#add_to_wishlist" + i).html("Remove from wishlist");
+					$("#add_to_wishlist" + i).css("background-color","red");
+					$("#add_to_watched" + i).css("display","none");
+				}
+			}
+
+});
+}
+function getAPI(arr) {
+
+//const my_array = Array.from(arr);
+console.log("in API " + arr);
+
+
+//document.getElementById("show_data").innerHTML = "";
+	let k = 0;
+	arr.forEach(element => {
+		let URL = "https://api.themoviedb.org/3/movie/" + element + "?api_key=fcabeffb7c941589973c5ba5beb7f636&language=en-US";
+		console.log("arr[i]: " + element);
+		res = fetch(URL)
+			.then (res => res.text())
+			.then (data => 
+			{
+				movie = JSON.parse(data)
+
+
+				let title = movie.original_title;
+				let genres = [];
+				for (let g = 0; g < movie["genres"].length; g++) {
+					genres[g] = movie["genres"][g];
+				}
+				
+				let img_source = movie["poster_path"];
+				let overview = movie["overview"];
+				let date = movie["release_date"];
+				let movie_id = movie["id"];
+
+				output(movie_id, k, title, img_source, genres, overview, date);
+				k++;
+				
+					
+				
+			})
+			.catch (error => console.log(error));
+	})
+	}
+
+			
+
+
+
+
+main();
+//console.log("arr is: " + arr);
+//getAPI(arr);
+
+</script>
+
+<?php
+include('./header.php');
+makeHeader('rec.php', 'Recommendations');
+?>
+
+<div id="show_data">
+	<p id="title"></p>
+	<p id="release"></p>
+	<p id="description"></p>
+	<p id="genres"></p>
+	
+</div>
 
 </body>
 </html>
+
+
